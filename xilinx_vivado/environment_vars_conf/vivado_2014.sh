b545b0fdbc5c3171264522625642853d3fb0cf49
Luis Paulo Fernandes de Barros <lbarros@cpqd.com.br>
2018-04-10 09:52:12 -0300

    Adding scripts to configure the environment variables for Vivado and the SDK

---
+++ xilinx_vivado/environment_vars_conf/vivado_2014.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source  /opt/Xilinx/Vivado/2014.3.1/settings64.sh
+source  /opt/Xilinx/SDK/2014.3.1/settings64.sh
+echo "========================================="
+echo "       VIVADO 2014.3.1 CONFIGURED"
+echo "========================================="
